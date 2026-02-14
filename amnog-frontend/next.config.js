8bb888e64519990dbaa5c8c6fa13d51fb29ea3e0
hhhirsch <hirsch.hans92@gmail.com>
2026-02-14 19:26:46 +0100

    next.config.js aktualisieren

--- amnog-frontend/next.config.js
+++ amnog-frontend/next.config.js
@@ -1,12 +1,14 @@
 const repo = "amnogtest";
+const isPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: isPages ? "export" : undefined, // Vercel: normales Next build
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+
+  basePath: isPages ? `/${repo}` : "",
+  assetPrefix: isPages ? `/${repo}/` : undefined,
 };
 
 module.exports = nextConfig;
