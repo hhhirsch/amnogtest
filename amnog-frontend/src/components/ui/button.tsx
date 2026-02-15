08d697e48f8a86570444a65b1a9207a99b538e8c
hhhirsch <hirsch.hans92@gmail.com>
2026-02-15 16:07:29 +0100

    button.tsx aktualisieren

--- amnog-frontend/src/components/ui/button.tsx
+++ amnog-frontend/src/components/ui/button.tsx
@@ -22,9 +22,19 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, ...props }, ref) => (
-  <button ref={ref} className={cn(buttonVariants({ variant, size, className }))} {...props} />
-));
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, type, ...props }, ref) => (
+    <button
+      ref={ref}
+      type={type ?? "button"}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+);
+
 Button.displayName = "Button";
