b4b5f2b6176f48f0af8b30f66b830d1fee01ec2b
hhhirsch <hirsch.hans92@gmail.com>
2026-02-17 11:25:18 +0100

    Add MONOTHERAPY role to TherapyRole enum

--- app/models.py
+++ app/models.py
@@ -36,8 +36,8 @@ class Setting(str, Enum):
 class TherapyRole(str, Enum):
     REPLACEMENT = "replacement"
     ADD_ON = "add-on"
+    MONOTHERAPY = "monotherapy"
     UNKLAR = "unklar"
-
 
 class TherapyLine(str, Enum):
     L1 = "1L"
